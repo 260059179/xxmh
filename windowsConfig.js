c294d8647690b3f2c1f837d3f39bb5b272628372
JY-AUDIO <li260059179@163.com>
2026-05-12 10:46:33 +0800

    Update windowsConfig.js

--- windowsConfig.js
+++ windowsConfig.js
@@ -1,2 +1,4 @@
 ntDownloadUrl:"https://cpv2.mairuan.com/mairuan.com/full/anydesk/AnyDesk.exe",
 ntDownloadX64Url:"https://cpv2.mairuan.com/mairuan.com/full/anydesk/AnyDesk.exe"
+ntDownloadX64Url:"https://cpv2.mairuan.com/mairuan.com/full/anydesk/AnyDesk.exe"
+ntDownloadX64Url:"https://cpv2.mairuan.com/mairuan.com/full/anydesk/AnyDesk.exe"
